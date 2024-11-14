209e4abb38f0239747d416875671a2d862e75f82
eliotourvieille <e.tourvieille@gmail.com>
2024-11-14 09:04:41 +0100

    Test auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Roboto_Condensed } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "./AuthProvider";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
 const roboto = Roboto_Condensed({
   weight: ["300", "400", "500", "600", "700"],
@@ -14,16 +15,22 @@ export const metadata: Metadata = {
   description: "Maîtrisez vos tournois. Optimisez vos gains.",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   
+  const { isAuthenticated } = getKindeServerSession();
+  const isAuthed = await isAuthenticated();
+
   return (
     <AuthProvider>
       <html lang="en" className="scroll-smooth">
-        <body className={roboto.className}>{children}</body>
+        <body className={roboto.className}>
+          {isAuthed && <div>Authed</div>}
+          {children}
+        </body>
       </html>
     </AuthProvider>
   );
